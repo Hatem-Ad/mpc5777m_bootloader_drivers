5d70be312171fb8ba69255aee8d55fae3ed87680
Hatem-Ad <45299723+Hatem-Ad@users.noreply.github.com>
2025-12-17 21:41:09 +0100

    Update bl_main.c

--- Bootloader/bl_main.c
+++ Bootloader/bl_main.c
@@ -3,8 +3,10 @@
 void Bootloader_Init()
 {
     /*  init clock, uart, gpio, flash */
+    BL_CLOCK_init();
     BL_GPIO_init_all();
     BL_UART_init();
+    BL_FLASH_init();
 }
 
 void Bootloader_Config()
